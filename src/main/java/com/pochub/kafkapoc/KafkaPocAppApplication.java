8f59e1432eae942903283009fcd498ad90dfb6a4
snmaddula <snmaddula@gmail.com>
2022-07-04 00:06:37 +0530

    ic

---
+++ src/main/java/com/pochub/kafkapoc/KafkaPocAppApplication.java
@@ -0,0 +1,13 @@
+package com.pochub.kafkapoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPocAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaPocAppApplication.class, args);
+	}
+
+}
